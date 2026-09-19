69961adf88e7ce87e5b2ddb5f2324d995abdfcd3
muano masiagwala <masiagwalamuano1@gmail.com>
2026-09-19 12:24:34 +0200

    refactor : greet to take a name argument

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 def greet_people():
     print("Hello, World!")
 
-def greet_person():
-    print("Hello, Alice")
+def greet_person(name):
+    print(f"Hello, {name}")
 
 
 greet_people()
-greet_person()
+greet_person("Alice")
+
